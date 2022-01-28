60593aa5d6068e227381b5c9684fb8736f0e13c4
Sergio Illán <illan2sergio@gmail.com>
2022-01-28 14:37:50 +0100

    First commit

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Sergio Illan Illan on 28/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
